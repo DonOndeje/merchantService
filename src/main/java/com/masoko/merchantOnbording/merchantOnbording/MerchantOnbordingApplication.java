b8061f81c6fc9f6dc59770f12eab6844f1bb1b0c
DonOndeje <donondeje@gmail.com>
2018-06-13 12:23:27 -0700

    initial commit

---
+++ src/main/java/com/masoko/merchantOnbording/merchantOnbording/MerchantOnbordingApplication.java
@@ -0,0 +1,12 @@
+package com.masoko.merchantOnbording.merchantOnbording;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantOnbordingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantOnbordingApplication.class, args);
+	}
+}
---
+++ src/main/java/restendpoint/ExposeRest.java
@@ -0,0 +1,9 @@
+//In this class we create a rest end pooint where all the other microservices are going to talk to this micro service.
+package restendpoint;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExposeRest {
+    
+}
