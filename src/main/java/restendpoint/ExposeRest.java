987722dda5b90893d0d8310bbc8299e71230dc65
DonOndeje <donondeje@gmail.com>
2018-06-13 15:11:49 -0700

    initialise the quert repository class

--- src/main/java/restendpoint/ExposeRest.java
+++ src/main/java/restendpoint/ExposeRest.java
@@ -1,4 +1,4 @@
-//In this class we create a rest end pooint where all the other microservices are going to talk to this micro service.
+//In this class we create a rest end point where all the other microservices are going to talk to this micro service.
 package restendpoint;
 
 import java.util.List;
@@ -6,15 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import repository.QueryRepository;
+
 
 @RestController
 @RequestMapping("/rest/merchantOnbording")
 
+  
 public class ExposeRest {
+    private QueryRepository queryRepository;
+    
      @GetMapping("/{username}")
       public List<String> getQuotes(@PathVariable("username") final String username){
           
           
+          
+          
+          // we need to query the database.
+          queryRepository.findByUsername(username);
           return null;
       }
 }
