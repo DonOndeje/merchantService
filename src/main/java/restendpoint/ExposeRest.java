070b3ee7c298df7bd88f3fb622b1b4127afaaa27
DonOndeje <donondeje@gmail.com>
2018-06-13 14:22:02 -0700

    Exposing the microservice for communication

--- src/main/java/restendpoint/ExposeRest.java
+++ src/main/java/restendpoint/ExposeRest.java
@@ -1,9 +1,20 @@
 //In this class we create a rest end pooint where all the other microservices are going to talk to this micro service.
 package restendpoint;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rest/merchantOnbording")
+
 public class ExposeRest {
-    
+     @GetMapping("/{username}")
+      public List<String> getQuotes(@PathVariable("username") final String username){
+          
+          
+          return null;
+      }
 }
